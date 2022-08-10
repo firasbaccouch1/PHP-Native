83a4bf552232d4200a303970ce5470176c13caaa
Firas <firasbaccouch5@gmail.com>
2022-08-10 06:46:07 +0200

    dot env

--- public/index.php
+++ public/index.php
@@ -9,6 +9,9 @@ require_once base_path() . 'router/web.php';
 
 use PHP\Http\Route;
 
+$dotenv = new Dotenv;
+$dotenv::createImmutable(base_path());
+$dotenv->load();
 $requst = new Request;
 $responce = new Response;
 $routesss = new Route($requst, $responce);
